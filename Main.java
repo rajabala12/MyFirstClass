c7383e216a7c1a4c8105012d298c2b701b191d79
rajabala12 <46661841+rajabala12@users.noreply.github.com>
2019-01-27 13:23:26 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    MyFirstClass myFirstClass = new MyFirstClass();
+        System.out.println("Count's value: " +myFirstClass.count);
+    }
+}
---
+++ MyFirstClass.java
@@ -0,0 +1,5 @@
+public class MyFirstClass {
+    int count = 20;
+    String name = "test class";
+    boolean flag = true;
+}
